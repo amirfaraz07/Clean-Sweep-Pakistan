9d551a17b2b6abaf27fd0ae77799e3c31dc523bb
amirfaraz07 <88924828+amirfaraz07@users.noreply.github.com>
2024-06-10 23:16:56 -0700

    Update config.js

--- cleansweeppakistan/src/config.js
+++ cleansweeppakistan/src/config.js
@@ -1,29 +1,3 @@
-// export const base_url = "http://localhost:5000/api"
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getStorage } from 'firebase/storage'
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDLf7uchCAwBQygJJHlt9wTKRK_F1M_nI0",
-//   authDomain: "clean-sweep-pakistan.firebaseapp.com",
-//   projectId: "clean-sweep-pakistan",
-//   storageBucket: "clean-sweep-pakistan.appspot.com",
-//   messagingSenderId: "570703408002",
-//   appId: "1:570703408002:web:045634e20c4e61fbfbfd9b",
-//   measurementId: "G-6ES0WPR6LH"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// export const imageDB = getStorage(app);
-
 export const base_url = "http://localhost:5000/api";
 
 // Import the functions you need from the SDKs you need
